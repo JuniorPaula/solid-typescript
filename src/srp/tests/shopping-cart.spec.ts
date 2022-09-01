a327365ecafd9d87a8c3370cb63894e12c20a7f4
Junior Paula <luke.junnior@icloud.com>
2022-09-01 16:58:26 -0300

    refactor: move sut creation to a factory method

--- src/srp/tests/shopping-cart.spec.ts
+++ src/srp/tests/shopping-cart.spec.ts
@@ -1,10 +1,20 @@
 import { CartItems } from '../entities/interfaces/shopping-cart-protocols';
 import { ShoppingCart } from '../entities/shopping-cart';
 
+const fakeItems = (): CartItems => ({
+  name: 'teste1',
+  price: 2.5,
+});
+
+const makeSut = (): ShoppingCart => {
+  const sut = new ShoppingCart();
+  return sut;
+};
+
 describe('ShoppingCart', () => {
   test('Should add items on success', async () => {
-    const sut = new ShoppingCart();
-    const items: CartItems = { name: 'teste1', price: 2.5 };
+    const sut = makeSut();
+    const items = fakeItems();
     sut.addItems(items);
     const result = sut.getItems();
     expect(result).toEqual([{ name: 'teste1', price: 2.5 }]);
