9ec06e2bd802b7c723b1f0790c57563e75ef599e
Junior Paula <luke.junnior@icloud.com>
2022-09-01 15:47:48 -0300

    test: ensure orderStatus to be open

---
+++ src/srp/tests/order.spec.ts
@@ -0,0 +1,72 @@
+import { Clear, IsEmpty } from '../entities/interfaces/shopping-cart-protocols';
+import { Order } from '../entities/order';
+import { SendMessageProtocols } from '../protocols/messaging';
+import { PersistencyProtocols } from '../protocols/persistency';
+
+const fakeIsEmpty = (): IsEmpty => {
+  class isEmptyStub implements IsEmpty {
+    isEmpty(): boolean {
+      return true;
+    }
+  }
+  return new isEmptyStub();
+};
+
+const fakeClear = (): Clear => {
+  class ClearStub implements Clear {
+    clear(): void {
+      console.log('Method not implemented.');
+    }
+  }
+  return new ClearStub();
+};
+
+const fakeMessaging = (): SendMessageProtocols => {
+  class MessagingStub implements SendMessageProtocols {
+    sendMessage(message: string): void {
+      console.log('Mensagem enviada: ' + message);
+    }
+  }
+  return new MessagingStub();
+};
+
+const fakePersistency = (): PersistencyProtocols => {
+  class PersistencyStub implements PersistencyProtocols {
+    saveOrder(): void {
+      console.log('Pedido salvo com sucesso!!!');
+    }
+  }
+  return new PersistencyStub();
+};
+
+type SutTypes = {
+  sut: Order;
+  isEmptyStub: IsEmpty;
+  clearStub: Clear;
+  messagingStub: SendMessageProtocols;
+  persistencyStub: PersistencyProtocols;
+};
+
+const makeSut = (): SutTypes => {
+  const isEmptyStub = fakeIsEmpty();
+  const clearStub = fakeClear();
+  const messagingStub = fakeMessaging();
+  const persistencyStub = fakePersistency();
+  const sut = new Order(isEmptyStub, clearStub, messagingStub, persistencyStub);
+
+  return {
+    sut,
+    isEmptyStub,
+    clearStub,
+    messagingStub,
+    persistencyStub,
+  };
+};
+
+describe('Order', () => {
+  test('Should return order status to be open', async () => {
+    const { sut } = makeSut();
+    const orderStatus = sut.getOrderStatus();
+    expect(orderStatus).toBe('open');
+  });
+});
